81e7b1a69d66db1bcdefdee466e092cf01ebf704
mclang <jani@kiho.fi>
2017-05-18 15:18:54 +0300

    Adds skeleton Reset Gnome Config script

---
+++ gnome_reset_config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# BEWARE:
+# Finds and deletes __ALL__ Gnome 3 things under user home directory!
+#
+
+# To back up and reset GNOME3:
+# mkdir ./.old-gnome-config/
+# mv ./.gnome* .gconf* ./.metacity ./.cache ./.dbus ./.dmrc ./.mission-control ./.thumbnails ~/.config/dconf/* ./.old-gnome-config/
+
+#
+# tar cvzf gnome-config-backup.tgz ~/.gnome* ~/.gconf* ~/.metacity ~/.cache ~/.dbus ~/.dmrc ~/.mission-control ~/.thumbnails ~/.config/dconf/
+#
+# tar cvzf gnome-config-backup.tgz ~/.gnome* ~/.dmrc ~/.thumbnails ~/.config/dconf/
+#
+
