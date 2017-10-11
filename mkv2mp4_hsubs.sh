b577088967eaf9e4a6a1ae92935a3dc0808efb7c
mclang <jani@kiho.fi>
2017-10-11 11:38:34 +0300

    Improves comments

--- mkv2mp4_hsubs.sh
+++ mkv2mp4_hsubs.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-# Converts given mkv files with __embedded__ subtitles into __harsubbed__ mp4 files using ffmpeg.
-# Uses hardlinking to overcome problem with spaces in filename of the source subtitle file.
+# Converts given mkv files with __embedded__ subtitles into __harsubbed__ mp4 files using 'ffmpeg'.
+# - Uses hardlinking to overcome problem with spaces in filename of the source subtitle file
+# - Using 'subtitles' filter needs that ffmpeg is compiled with '--enable-libass'
+#
+# https://trac.ffmpeg.org/wiki/HowToBurnSubtitlesIntoVideo
+# http://ffmpeg.org/ffmpeg-filters.html#subtitles-1
 #
 set -u
 set -e
@@ -8,10 +12,11 @@ shopt -s nullglob
 
 for MKV in "$@"; do
 	out="$(basename "$MKV" .mkv).mp4"
-	echo "CONVERTING '$MKV''"
-	cp -l "$MKV" input.mkv
-	ffmpeg -i input.mkv -c:a copy -c:v libx264 -profile:v high -level:v 4.1 -preset medium -crf 23 -vf subtitles=input.mkv "$out"
+	echo "CONVERTING '$MKV' -> '$out'"
+	cp -al "$MKV" input.mkv
+	ffmpeg -i input.mkv -c:a copy -c:v libx264 -profile:v high -level:v 4.2 -preset medium -crf 23 -vf subtitles=input.mkv "$out"
 	rm -f input.mkv
 	echo "==> $out"
 	echo ""
 done
+
