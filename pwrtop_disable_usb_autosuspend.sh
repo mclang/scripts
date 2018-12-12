8a4df6fe067b13213fcf4db651a1737a1f60f87b
mclang <jani.long@gmail.com>
2018-12-12 10:24:20 +0200

    Adds 'Logitech Anywhere MX'

--- pwrtop_disable_usb_autosuspend.sh
+++ pwrtop_disable_usb_autosuspend.sh
@@ -3,8 +3,14 @@
 # https://www.kirsle.net/wiki/PowerTOP-and-USB-Autosuspend
 # https://hamwaves.com/usb.autosuspend/en/
 #
-# Disables USB auto-suspend for the configured devices. Does NOT survive
-# reboot though, so you might be better of creating UDEV rules like this:
+# Disables USB auto-suspend for the configured devices. To find the right device and
+# path, run `sudo powertop`, find the right entry and toggle it in to 'bad' state.
+# When toggling the state powertop displays what it is doing, like
+# ```
+# echo 'on' > '/sys/bus/usb/devices/2-3.2.3.1/power/control'
+# ```
+#
+# This does NOT survive reboot though, so you might be better of creating UDEV rules like this:
 # ```
 # # Bus 003 Device 007: ID 10f5:0651 Turtle Beach
 # ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="10f5", ATTR{idProduct}=="0651", TEST=="power/control", ATTR{power/control}="on"
@@ -18,6 +24,7 @@ set -u
 set -e
 
 declare -A DEVICES=(
+	['Logitech Anywhere MX']='/sys/bus/usb/devices/1-1/power/control'
 	['Logitech M570']='/sys/bus/usb/devices/3-1.7/power/control'
 	['Turtlebeach Impact 500']='/sys/bus/usb/devices/3-1.6/power/control'
 )
