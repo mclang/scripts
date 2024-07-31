e99f86bb17fdab8d08a00b7fd5b69a93781c91dd
mclang <jani@kiho.fi>
2024-07-31 21:50:29 +0300

    Changes 'desktop background changer' to work with Sway

--- dm-de-wm/desktop_background_changer.sh
+++ dm-de-wm/desktop_background_changer.sh
@@ -1,22 +1,49 @@
 #!/bin/bash
-# Changes desktop background using Nitrogen.
-# Can be used directly, or by using systemd user service/timer files
+#
+# Changes desktop background using `swaymsg`/`swaybg`.
+#
+# Can be used directly, or by using systemd USER service/timer files
 # defined in `$HOME/.config/systemd/user`
 #
-BACKGROUNDS="/home/mclang/Pictures/Wallpapers"
-if [[ ! -d "$BACKGROUNDS" ]]; then
-	echo "ERROR: Directory '$BACKGROUNDS' does not exist!"
+# TODO:
+# - Maybe try https://github.com/xyproto/wallutils
+# - Use `swaymsg -t get_outputs` to set different backgrounds for different monitors
+# - https://github.com/swaywm/swaybg/issues/64
+#
+# NOTE: Cannot be enabled b/c `pidof swaybg` errors if process not found!
+# set -euo pipefail
+
+WALLPAPERS="/home/mclang/Pictures/Wallpapers"
+if [[ ! -d "$WALLPAPERS" ]]; then
+	echo "ERROR: Directory '$WALLPAPERS' does not exist!"
 	exit 1
 fi
-if ! hash xrandr 2>/dev/null; then
-	echo "ERROR: Command 'xrandr' cannot be found!"
+if ! hash shuf 2>/dev/null; then
+	echo "ERROR: Command 'shuf' cannot be found!"
 	exit 1
 fi
-if ! hash nitrogen 2>/dev/null; then
-	echo "ERROR: Command 'nitrogen' cannot be found!"
+if ! hash swaybg 2>/dev/null; then
+	echo "ERROR: Command 'swaybg' cannot be found!"
 	exit 1
 fi
-for HEAD in `xrandr --listactivemonitors | sed -n 's/\s*\([0-9]\):\s*.*/\1/p'`; do
-	nitrogen --head=$HEAD --set-zoom-fill --random "$BACKGROUNDS" 2>/dev/null
-done
+if ! hash swaymsg 2>/dev/null; then
+	echo "ERROR: Command 'swaymsg' cannot be found!"
+	exit 1
+fi
+if [[ -z "$SWAYSOCK" ]]; then
+	echo "INFO: Skipping desktop background change b/c 'SWAYSOCK' environment variable NOT defined"
+	exit 0
+fi
+
+BACKGROUND="$(find "$WALLPAPERS" -name '*.jpg' -o -name '*.png' | shuf -n1)"
+
+# EITHER (shows grey screen for a moment...):
+swaymsg -s "$SWAYSOCK" output '*' bg "$BACKGROUND" fill '#000000'
+
+# OR (does not work as systemd user service...):
+#OLD_PIDS="$(pidof swaybg)"
+#swaybg -i "$BACKGROUND" -m fill -o '*' &
+#if [[ -n "$OLD_PIDS" ]]; then
+#	kill $OLD_PIDS
+#fi
 
